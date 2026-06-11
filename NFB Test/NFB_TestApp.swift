c205f6c18c207c5b197aaa67152a358957e70995
Vatsalya Dabhi <vatsalyadabhi2003@gmail.com>
2026-06-11 15:15:33 -0700

    Initial Commit

---
+++ NFB Test/NFB_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFB_TestApp.swift
+//  NFB Test
+//
+//  Created by Vatsalya's Mac on 6/11/26.
+//
+
+import SwiftUI
+
+@main
+struct NFB_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NFB TestTests/NFB_TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  NFB_TestTests.swift
+//  NFB TestTests
+//
+//  Created by Vatsalya's Mac on 6/11/26.
+//
+
+import Testing
+@testable import NFB_Test
+
+struct NFB_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
